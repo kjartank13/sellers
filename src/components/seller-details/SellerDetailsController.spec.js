86fcc85f6f03f8c458e47b1e66aca1d50f824ef2
thoddi <thorvardur13@ru.is>
2016-03-13 21:33:02 +0000

    baetti vid unit test fyrir seller details

---
+++ src/components/seller-details/SellerDetailsController.spec.js
@@ -0,0 +1,33 @@
+"use strict";
+
+describe("SellersDetailsController unit tests", function() {
+
+	beforeEach(module("project3App"));
+
+	var AppResource;
+	var newController;
+	var scope;
+	var $location;
+
+	beforeEach(inject(function($rootScope, $controller, $injector, _$location_) {
+		scope = $rootScope.$new();
+		$location = _$location_;
+		AppResource = $injector.get("AppResource");
+		newController = $controller("SellerDetailsController", { 
+				$scope: scope,
+				AppResource: AppResource
+			});
+
+		
+	}));
+
+	it ("should successfully get list of products", function () {
+			expect(scope.allProducts).toBeDefined();
+		});
+
+	it ("should define onAddProduct function", function() { 
+			expect(scope.onAddProduct).toBeDefined();
+		});
+
+});
+
--- src/components/sellers/SellersController.spec.js
+++ src/components/sellers/SellersController.spec.js
@@ -1,5 +1,33 @@
 "use strict";
 
-describe("SellersController should be unit tested here", function() {
-	// TODO: add beforeEach/describe/it/etc. functions as appropriate!
+describe("SellersController unit tests", function() {
+
+	beforeEach(module("project3App"));
+
+	var AppResource;
+	var newController;
+	var scope;
+	var $location;
+
+	beforeEach(inject(function($rootScope, $controller, $injector, _$location_) {
+		scope = $rootScope.$new();
+		$location = _$location_;
+		AppResource = $injector.get("AppResource");
+		newController = $controller("SellersController", { 
+				$scope: scope,
+				AppResource: AppResource
+			});
+
+		
+	}));
+
+	it ("should successfully get list of sellers", function () {
+			expect(scope.sellers).toBeDefined();
+		});
+
+	it ("should define onAddSeller function", function() { 
+			expect(scope.onAddSeller).toBeDefined();
+		});
+
 });
+
